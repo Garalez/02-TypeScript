6efc0bfce7b90f9e11a500001459ae4856520a1a
Vlad Galcev <Vladgalcev03@gmail.com>
2022-11-08 16:37:00 +0200

    Модуль 10 Тема 2 Основные типы JS в TS

--- src/module/verdict.ts
+++ src/module/verdict.ts
@@ -3,12 +3,12 @@ import { car } from "./car";
 const totalVerdict = (car: {
   rating: [string, number][];
 }): number => {
-  const sumRating = car.rating.reduce((acc: number, item: [string, number]) => {
+  const sumRating: number = car.rating.reduce((acc: number, item: [string, number]) => {
     console.log(acc, item);
 
     return item[1] + acc;
   }, 0);
-  const verdict = sumRating / car.rating.length;
+  const verdict: number = sumRating / car.rating.length;
   return verdict;
 };
 
